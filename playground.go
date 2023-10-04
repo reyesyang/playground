29f9832cd144ec8866819934ad851fb1ee359091
yanghailong <reyesyang@foxmail.com>
2023-10-04 19:21:10 +0800

    Feature: Init project

---
+++ playground.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"content": "playground",
+		})
+	})
+
+	r.Run(":8080")
+}
